52ec8835915ee7f554d797e55c3804f1b999ded4
Kushagra Gupta <guptakushagra15.10@gmail.com>
2022-04-05 15:50:53 +0530

    🎉 Initial commit, add prexisting files

---
+++ src/utils/dummy/latestTransactions.js
@@ -0,0 +1,33 @@
+export const latestTransactionsData = [
+    {
+      id: 1,
+      transaction: "Hostel Fee 2021",
+      paid_on: "20 Feb 2021",
+      amount: "₹200000",
+    },
+    {
+      id: 2,
+      transaction: "Tuition Fee 2021",
+      paid_on: "20 Feb 2021",
+      amount: "₹200000",
+    },
+    {
+      id: 3,
+      transaction: "Exam Fee 2021",
+      paid_on: "1 Feb 2021",
+      amount: "₹5000",
+    },
+    {
+      id: 4,
+      transaction: "Sports Fee",
+      paid_on: "3 March 2021",
+      amount: "₹20000",
+    },
+    {
+      id: 5,
+      transaction: "Library Fee",
+      paid_on: "10 March 2021",
+      amount: "₹20000",
+    },
+  ];
+  
