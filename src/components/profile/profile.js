9a4c5e8fbd3908893624659f73a537059b3f1868
Kushagra Gupta <guptakushagra15.10@gmail.com>
2022-04-10 09:06:49 +0530

    ✨ Add profilecard, profile page

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -11,8 +11,30 @@ export default function ProfileComponent() {
           className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64"
         >
           <main>
-            <div className="pt-6 px-4">
-              <h1>My Profile</h1>
+            <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
+              <h3 className="text-xl font-bold text-gray-900 mb-2">
+                My Profile
+              </h3>
+              <div className="flex flex-col lg:flex-row my-5">
+                <img
+                  className="w-36 h-36 rounded-full mx-auto lg:mx-0"
+                  src="https://media-exp1.licdn.com/dms/image/C5603AQElOHX5fWfC1w/profile-displayphoto-shrink_400_400/0/1645607283072?e=1655337600&v=beta&t=2HAQOOrx8vw8JjfwPRJLnmtCZxECxRPTZRawO6JTOLM"
+                  alt="profile-pic"
+                />
+                <div className="mx-10">
+                  <p className="text-lg font-bold my-3">Name: Kushagra Gupta</p>
+                  <p my-3>Branch: B.tech CSE</p>
+                  <p my-3>Specialization: AIML</p>
+                  <p my-3>Email ID: kg8147@srmist.edu.in</p>
+                  <p my-3>Password: **********</p>
+                </div>
+              </div>
+
+              <div className="flex items-center justify-center">
+                <button className="bg-blue-500 hover:opacity-90 text-white px-5 py-2 rounded-md my-10 ">
+                  Save Changes
+                </button>
+              </div>
             </div>
           </main>
 
